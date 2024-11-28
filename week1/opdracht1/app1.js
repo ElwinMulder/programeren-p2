78a05326df49944e5ab82d8c993dadc7a609c0ac
ElwinMulder <9025151@student.zadkine.nl>
2024-11-28 10:17:04 +0100

    week 1

---
+++ week1/opdracht1/app1.js
@@ -0,0 +1,11 @@
+var modal = document.getElementById("cookiewall");
+
+function showmodal(){
+    modal.style.display = "block";
+}
+
+function removeModal(){
+    modal.style.display = "none";
+}
+
+showmodal();
